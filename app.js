3f024e75da324a3d102696030cdd9490ffcdf0cb
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2024-02-22 09:00:56 +0530

    Login still in progress

--- app.js
+++ app.js
@@ -4,9 +4,33 @@ const bodyParser = require('body-parser');
 const cors = require('cors'); 
 
 const app = express();
-const PORT = 3001;
 
+require('dotenv').config();
+require('express-async-errors');
+//connectDB
 app.use(cors());
+const connectDB = require('./db/connect');
+// const authenticateUser = require('./middleware/authentication');
+//routers
+const authRouter = require('./routes/auth')
+// const jobsRouter = require('./routes/jobs')
+
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.use('/api/v1/auth',authRouter);
+// app.use('/api/v1/jobs',authenticateUser,jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
 app.use(bodyParser.json());
 
 app.post('/writeCartToJson', (req, res) => {
@@ -36,6 +60,18 @@ app.get('/cartData', (req, res) => {
     }
   });
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+const port = process.env.PORT || 3001;;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
